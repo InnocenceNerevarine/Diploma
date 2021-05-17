cd7a4ddfe6339e2fe66d457a4a7c69a55334be38
Илья <ilya.sidorov.2014@gmail.com>
2021-05-17 18:39:20 +0300

    GIT init

---
+++ MainApp/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+
+from .models import *
+
+
+class ProductAdmin(admin.ModelAdmin):
+    change_form_template = 'custom_admin/change_form.html'
+    #exclude = ('features',)
+
+
+class SlugifyProduct(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Category)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Product, ProductAdmin)
